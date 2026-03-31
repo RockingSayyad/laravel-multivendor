ad9ca50b8a76e40382154c9f6119ae9adc61be72
RockingSayyad <sayyadhamza451@gmail.com>
2026-03-31 22:04:18 +0530

    sanctum unauthenicated changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
 
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+   ->withMiddleware(function (Middleware $middleware) {
+    $middleware->redirectGuestsTo(function () {
+        return response()->json([
+            'message' => 'Unauthenticated.'
+        ], 401);
+    });
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
